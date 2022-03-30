9ec3bdeacf6a26fd1cba50e4ec233b5c948e9e3f
Yashit <veeryashit@gmail.com>
2022-03-30 10:24:36 +0530

    Completed

--- src/App.js
+++ src/App.js
@@ -1,14 +1,29 @@
 import Navbar from "./components/Navbar";
 import Hero from "./components/Hero";
 import Card from "./components/Card";
+import data from "./components/data";
 
 function App() {
+  const cardData = data.map((data) => {
+    return (
+      <Card
+        key={data.id}
+        img={data.coverImg}
+        stars={data.stats.rating}
+        reviews={data.stats.reviewCount}
+        country={data.location}
+        heading={data.title}
+        cost={`$${data.price}`}
+        availability={data.openSpots}
+      />
+    );
+  });
   return (
     <div className="App">
       <header className="App-header">
         <Navbar />
         <Hero />
-        <Card />
+        <div className="container-lessons">{cardData}</div>
       </header>
     </div>
   );
--- src/components/Card.js
+++ src/components/Card.js
@@ -1,25 +1,34 @@
 import React from "react";
-import photo1 from "../images/photo-1.png";
 import star from "../images/star.png";
 
-export default function Card() {
+export default function Card({
+  img,
+  stars,
+  reviews,
+  country,
+  heading,
+  cost,
+  availability,
+}) {
+  const available = availability ? "Available" : "Sold Out";
+  const photo1 = require(`../images/${img}`);
   return (
     <div className="card">
       <div className="image-container">
-        <p className="btn btn-light">Sold Out</p>
+        <p className="btn btn-light">{available}</p>
         <img src={photo1} className="card-image" />
       </div>
       <div className="rating">
         <img src={star} className="" />
         <p>
-          <span className="rating-heading">5.0 </span>
-          <span>(6) • </span>
-          <span>USA</span>
+          <span className="rating-heading">{stars} </span>
+          <span>({reviews}) • </span>
+          <span>{country}</span>
         </p>
       </div>
-      <h3 className="card-heading">Life lessons with Katie Zaferes</h3>
+      <h3 className="card-heading">{heading}</h3>
       <p>
-        <span className="card-bold">From $136</span> / Per Person
+        <span className="card-bold">From {cost}</span> / Per Person
       </p>
     </div>
   );
